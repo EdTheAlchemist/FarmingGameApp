84fb8ddbf82887de725bb3c2849dce9ffdc0a64f
Ed <edward.tighe@dlsu.edu.ph>
2021-08-30 15:48:52 +0800

    Added comments

--- app/src/main/java/com/mobdeve/tighee/farminggameapp/MainActivity.java
+++ app/src/main/java/com/mobdeve/tighee/farminggameapp/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
@@ -25,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
+    // Views for the activity
     private TextView moneyTv;
     private Button buyBtn;
     private LinearLayout currentProductHll;
@@ -33,14 +35,17 @@ public class MainActivity extends AppCompatActivity {
     private ScrollView gameLogSv;
     private ProgressBar progressBar;
 
+    // Keeps track of our money
     private int money;
+    // Keeps track of the number of jobs sent off. doubles as the job ID.
     private int jobCount;
 
+    // Components needed for the BroadcastReceiver
     private boolean registeredReceiver = false;
-
     private BroadcastReceiver myReceiver;
     private IntentFilter myFilter;
 
+    // Component for handling the queuing of background tasks
     private ExecutorService executorService;
 
     @Override
@@ -48,8 +53,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Here we define an ExecutorService, which will help us in managing incoming tasks. We
+        // use a single thread executor to force all tasks to be executed in a queue like fashion.
         this.executorService = Executors.newSingleThreadExecutor();
 
+        // View initialization
         this.moneyTv = findViewById(R.id.moneyTv);
         this.buyBtn = findViewById(R.id.buyBtn);
         this.currentProductHll = findViewById(R.id.currentProductHll);
@@ -57,20 +65,33 @@ public class MainActivity extends AppCompatActivity {
         this.gameLogSv = findViewById(R.id.gameLogSv);
         this.currentProductVll = findViewById(R.id.currentProductVll);
 
-        this.money = 30;
+        // Initialization of the money and its view
+        this.money = 300;
         this.moneyTv.setText("Money: " + this.money);
 
+        // Need I explain this?
         this.jobCount = 1;
 
+        // See method below for explanation. TLDR; it sets viws for when there's no task
         addNoTaskView();
 
         this.buyBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // Checks if the use has enough money
                 if(money >= 10) {
+                    // Generic function to modify the value and the view
                     updateMoney(-10);
+
+                    // Here, we create our ProductionRunnable object, which handles the tasks
+                    // needed. See ProductionRunnable.java for more info.
                     executorService.execute(new ProductionRunnable(jobCount, MainActivity.this));
+
+                    // Once a task task has been scheduled, we add to the game log
                     addToGameLog(jobCount, "Production task queued.");
+
+                    // Increment the job count; As this is also the job ID, we'd want this to be
+                    // unique
                     jobCount++;
                 } else {
                     Toast.makeText(
@@ -81,6 +102,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Logic for the BroadcastReceiver. We use the IntentFilter to focus the receiver to only
+        // specific actions.
         this.myReceiver = new MyBroadcastReceiver();
         this.myFilter = new IntentFilter();
         this.myFilter.addAction(Constants.START_INTENT_ACTION);
@@ -88,6 +111,7 @@ public class MainActivity extends AppCompatActivity {
         this.myFilter.addAction(Constants.FINISH_INTENT_ACTION);
     }
 
+    // We turn the receiver "on" in case the receiver had been turned off or on initialization.
     @Override
     protected void onStart() {
         super.onStart();
@@ -97,6 +121,8 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    // We turn the receiver "off" when the user is leaving as we don't want the receiver listening
+    // when the user is not interacting with the activity.
     @Override
     protected void onPause() {
         super.onPause();
@@ -106,12 +132,17 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    // This is just so we can have the log tell us when the activity is destroyed and be able to
+    // observe how other processes might still be running. Ideally, we wouldn't want to see anything
+    // after onDestroy is called.
     @Override
     protected void onDestroy() {
         super.onDestroy();
         Log.d(TAG, "onDestroy: called");
     }
 
+    // This handles adding the appropriate views informing the user that there are no tasks pending.
+    // This can happen on start or when all tasks actuall finish and a lull is present.
     private void addNoTaskView() {
         TextView tv = new TextView(this);
         tv.setText("No current task running.");
@@ -120,6 +151,9 @@ public class MainActivity extends AppCompatActivity {
         this.currentProductVll.setMinimumHeight(220);
     }
 
+    // We aren't doing anything complex with the game log and simply append a new TextView with an
+    // incoming message. The jobCount variable is needed so we know which job is currently doing
+    // something.
     private void addToGameLog(int jobCount, String message) {
         String log = "Task #" + jobCount + ": " + message;
 
@@ -127,54 +161,19 @@ public class MainActivity extends AppCompatActivity {
         temp_tv.setText(log);
 
         gameLogVll.addView(temp_tv);
+        gameLogSv.fullScroll(ScrollView.FOCUS_DOWN);
     }
 
+    // A simple method that simplifies modifying the money and its view. This is done both on buying
+    // a task and on receiving that a task had been completed.
     private void updateMoney(int m) {
         this.money = this.money + m;
         this.moneyTv.setText("Money: " + this.money);
     }
 
-    private void scrollToBottom() {
-        gameLogSv.post(new Runnable() {
-            @Override
-            public void run() {
-                gameLogSv.fullScroll(ScrollView.FOCUS_DOWN);
-            }
-        });
-    }
-
-    public class MyBroadcastReceiver extends BroadcastReceiver {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            Log.d(TAG, "onReceive:");
-
-            int jobCount = intent.getIntExtra(Constants.JOB_ID_KEY, 0);
-            String productName = intent.getStringExtra(Constants.NAME_KEY);
-
-            if(intent.getAction().equals(Constants.START_INTENT_ACTION)) {
-                int imageId = intent.getIntExtra(Constants.IMAGE_KEY, 0);
-                int productionTime = intent.getIntExtra(Constants.TIME_KEY, 0);
-
-                addToGameLog(jobCount, "Work on " + productName + " product started.");
-                addCurrentTaskViews(imageId, productName, productionTime);
-
-                scrollToBottom();
-            } else if(intent.getAction().equals(Constants.FINISH_INTENT_ACTION)) {
-                int generatedMoney = intent.getIntExtra(Constants.MONEY_KEY, 0);
-
-                updateMoney(generatedMoney);
-                addToGameLog(jobCount, "Work on " + productName + " has finished. Generated " + generatedMoney + " money.");
-                removeCurrentTaskViews();
-
-                scrollToBottom();
-            } else if(intent.getAction().equals(Constants.PROGRESS_INTENT_ACTION)) {
-                int currProgress = intent.getIntExtra(Constants.PROGRESS_KEY, 0);
-
-                progressBar.setProgress(currProgress);
-            }
-        }
-    }
-
+    // This method handles creating the views for showing a task is being worked on.
+    // This was an older project that I decided to update and I didn't want to to through the hassle
+    // of changing this to a fragment. Still, this can be optimized.
     private void addCurrentTaskViews(int imageId, String productName, int productionTime) {
         this.currentProductHll.removeAllViews();
 
@@ -196,6 +195,7 @@ public class MainActivity extends AppCompatActivity {
         this.currentProductVll.addView(progressBar);
     }
 
+    // This handles removing the current task views.
     private void removeCurrentTaskViews() {
         this.currentProductHll.removeAllViews();
 
@@ -204,5 +204,45 @@ public class MainActivity extends AppCompatActivity {
         View v = this.currentProductVll.getChildAt(0);
         this.currentProductVll.removeAllViews();
         this.currentProductVll.addView(v);
+    }
+
+    /*
+     * This is our custom BroadcastReceiver class. Its job is to listen for when the background task
+     *  finishes something. It basically responds to when the task starts, is being worked on, and
+     * finishes. It mainly handles making changes to the UI. Logic here runs on the main thread.
+     * */
+    public class MyBroadcastReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d(TAG, "onReceive:");
+
+            // All incoming intents  had a jobCount and productName, so we get them here.
+            int jobCount = intent.getIntExtra(Constants.JOB_ID_KEY, 0);
+            String productName = intent.getStringExtra(Constants.NAME_KEY);
+
+            if(intent.getAction().equals(Constants.START_INTENT_ACTION)) {
+                // For when the intent was for a production start. Here, we get the information
+                // needed to display that a task is being worked on.
+                int imageId = intent.getIntExtra(Constants.IMAGE_KEY, 0);
+                int productionTime = intent.getIntExtra(Constants.TIME_KEY, 0);
+
+                addToGameLog(jobCount, "Work on " + productName + " product started.");
+                addCurrentTaskViews(imageId, productName, productionTime);
+            } else if(intent.getAction().equals(Constants.FINISH_INTENT_ACTION)) {
+                // When a task has finished, we want to get the money generated and update the UI
+                // accordingly.
+                int generatedMoney = intent.getIntExtra(Constants.MONEY_KEY, 0);
+
+                updateMoney(generatedMoney);
+                addToGameLog(jobCount, "Work on " + productName + " has finished. Generated " + generatedMoney + " money.");
+                removeCurrentTaskViews();
+            } else if(intent.getAction().equals(Constants.PROGRESS_INTENT_ACTION)) {
+                // For handling updates. This happens every second due to the logic in our Runnable
+                // object
+                int currProgress = intent.getIntExtra(Constants.PROGRESS_KEY, 0);
+
+                progressBar.setProgress(currProgress);
+            }
+        }
     }
 }
--- app/src/main/java/com/mobdeve/tighee/farminggameapp/ProductionRunnable.java
+++ app/src/main/java/com/mobdeve/tighee/farminggameapp/ProductionRunnable.java
@@ -4,6 +4,22 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
+/*
+ * On initialization, our custom Runnable object needs a job ID, which it would use to broadcast its
+ * current progress to the log. This handles the main logic of the task, which involves (1) creating
+ * a random product instance, (2) running the Thread.sleep to simulate working on the task, and (3)
+ * finishing the task. At each step, the runnable utilizes sendBroadcast method call to send updates
+ * to the UI thread -- where our MyBroadcastReceiver is waiting. The logic for randomly generating
+ * a product and randomly generating the output money is found in Product.java.
+ *
+ * In comparison to the other solution to this game app, we don't use handlers but utilize an
+ * indirect bridge to the main thread -- broadcasts -- to update the UI. The BroadcastReceiver runs
+ * on the main thread.
+ *
+ * Side note: I could have simplified the "putting" of information into the intents by not creating
+ * a new intent all the time. The job and name are repeating and won't handle the naman, so this is
+ * possible, but so things are clear, I decided to not do that.
+ * */
 public class ProductionRunnable implements Runnable {
     private final static String TAG = "ProductionTaskLog";
 
@@ -37,7 +53,6 @@ public class ProductionRunnable implements Runnable {
         i.putExtra(Constants.TIME_KEY, product.getProductionTime());
         i.putExtra(Constants.JOB_ID_KEY, this.taskId);
         this.context.sendBroadcast(i);
-
 
         // Work on the actual product (i.e. sleep...) + send updates
         for(int ctr = 0; ctr < product.getProductionTime(); ctr++) {
